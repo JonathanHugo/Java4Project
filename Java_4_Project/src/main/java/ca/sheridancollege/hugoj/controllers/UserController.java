3d06addcd8076eb2644501cbd8abf9cfc1a9b7b8
ranvi-singh <60109297+ranvi-singh@users.noreply.github.com>
2022-04-02 00:44:15 -0400

    UserController

---
+++ Java_4_Project/src/main/java/ca/sheridancollege/hugoj/controllers/UserController.java
@@ -0,0 +1,54 @@
+package ca.sheridancollege.hugoj.controllers;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ca.sheridancollege.hugoj.domain.Account;
+import ca.sheridancollege.hugoj.domain.Game;
+import ca.sheridancollege.hugoj.domain.GamePlay;
+import ca.sheridancollege.hugoj.domain.User;
+import ca.sheridancollege.hugoj.repositories.AccountRepository;
+import ca.sheridancollege.hugoj.repositories.GamePlayRepository;
+import ca.sheridancollege.hugoj.repositories.UserRepository;
+
+
+@Controller
+public class UserController {
+	
+	UserRepository userRepository;
+	AccountRepository accountRepository;
+	GamePlayRepository gamePlayRepository;
+	
+	@PostMapping("/addUser")
+	public String addUser(Model model,@ModelAttribute User user) {
+		
+		User u = userRepository.save(user);
+		Account a = new Account();
+		a.setUser(u);
+		return "index";
+	}
+	
+	@PostMapping("/addUserWithGamePlays")
+	public String addUserWithGamePlays(Model model,@RequestParam String idForUser,@ModelAttribute GamePlay gamePlay) {
+
+		GamePlay g = gamePlayRepository.save(gamePlay);
+		User u = userRepository.findUserById(Long.parseLong(idForUser));
+		u.getGamePlays().add(g);
+		userRepository.save(u);
+		return "index";
+	}
+	
+	@GetMapping("/displayUser")
+	public String displayUser(Model model,@ModelAttribute User user) {
+		
+		userRepository.findAll();
+		return "index";
+	}
+
+}
--- Java_4_Project/src/main/java/ca/sheridancollege/hugoj/repositories/UserRepository.java
+++ Java_4_Project/src/main/java/ca/sheridancollege/hugoj/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import ca.sheridancollege.hugoj.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	public User findUserById(Long id);
+	
 }
