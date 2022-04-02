cb6753fde6cb5c85e977a48696bf6da55b3a0ea8
Gabriel <espingab@sheridancollege.ca>
2022-04-02 14:15:41 -0400

    GameController layout - Gabriel

--- Java_4_Project/src/main/java/ca/sheridancollege/hugoj/controllers/GameController.java
+++ Java_4_Project/src/main/java/ca/sheridancollege/hugoj/controllers/GameController.java
@@ -1,8 +1,55 @@
 package ca.sheridancollege.hugoj.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ca.sheridancollege.hugoj.domain.Account;
+import ca.sheridancollege.hugoj.domain.User;
+import ca.sheridancollege.hugoj.repositories.AccountRepository;
+import ca.sheridancollege.hugoj.repositories.GamePlayRepository;
+import ca.sheridancollege.hugoj.repositories.UserRepository;
 
 @Controller
 public class GameController {
 
+	UserRepository userRepository;
+	AccountRepository accountRepository;
+	GamePlayRepository gamePlayRepository;
+	
+	@GetMapping("/displayUsers")
+	public String displayUsers(Model model) {
+		
+		model.addAttribute("userList", gamePlayRepository.getById(null));
+		
+		return "leaderboard";
+	}
+	
+	@PostMapping("/addUserToGame")
+	public String addUser(Model model, @ModelAttribute User user) {
+		
+		
+		
+		
+		
+		return "gamepage";
+		
+	}
+	
+	@PostMapping("/updateScoreToUser")
+	public String addScore(Model model) {
+		
+		return "gamepage";
+	}
+	
+	@PostMapping("/uploadUserScore")
+	public String uploadUserScore(Model model) {
+		
+		
+		return "index";
+	}
+	
+	
 }
